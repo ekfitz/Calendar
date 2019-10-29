38e236c7103056d7e59f42199ffe0d0e103b8a89
ekfitz <51545728+cultofsnake@users.noreply.github.com>
2019-10-29 16:02:57 -0700

    testing array slice

--- js/cy20.js
+++ js/cy20.js
@@ -1,3 +1,4 @@
+var cy18 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 var cy19 = [0,
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,
@@ -28,6 +29,19 @@ var dOy = [0,
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
 ];
+
+var jan19 = cy19.slice(1, 34);
+var feb19 = cy19.slice(27, 62);
+var mar19 = cy19.slice(55, 90);
+var apr19 = cy19.slice(90, 125);
+var may19 = cy19.slice(118, 153);
+var jun19 = cy19.slice(146, 181);
+var jul19 = cy19.slice(181, 216);
+var aug19 = cy19.slice(209, 244);
+var sep19 = cy19.slice(244, 279);
+var oct19 = cy19.slice(272, 307);
+var nov19 = cy19.slice(300, 335);
+var dec19 = cy19.slice(335, 370);
 
 var duJan19 = 0;
 var duFeb19 = duJan19 + 31;
@@ -61,35 +75,67 @@ var d = new Date();
 var thisMonth = d.getMonth();
 var today = d.getDate();
 
-function firstBox(x) {
-  if (x == 0) {
-    return duDec + 27
-  } else if (x == 1) {
-    return duJan19 + 27;
-  } else if (x == 2) {
-    return duFeb19 + 24;
-  } else if (x == 3) {
-    return duMar19 + 31;
-  } else if (x == 4) {
-    return duApr19 + 28;
-  } else if (x == 5) {
-    return duMay19 + 26;
-  } else if (x == 6) {
-    return duJun19 + 30;
-  } else if (x == 7) {
-    return duJul19 + 28;
-  } else if (x == 8) {
-    return duSep19 + 1;
-  } else if (x == 9) {
-    return duSep19 + 29;
-  } else if ( x == 10) {
-    return duOct19 + 27;
+var d1 = fillMonth(thisMonth)[0];
+var d2 = fillMonth(thisMonth)[1];
+var d3 = fillMonth(thisMonth)[2];
+var d4 = fillMonth(thisMonth)[3];
+var d5 = fillMonth(thisMonth)[4];
+var d6 = fillMonth(thisMonth)[5];
+var d7 = fillMonth(thisMonth)[6];
+var d8 = fillMonth(thisMonth)[7];
+var d9 = fillMonth(thisMonth)[8];
+var d10 = fillMonth(thisMonth)[9];
+var d11 = fillMonth(thisMonth)[10];
+var d12 = fillMonth(thisMonth)[11];
+var d13 = fillMonth(thisMonth)[12];
+var d14 = fillMonth(thisMonth)[13];
+var d15 = fillMonth(thisMonth)[14];
+var d16 = fillMonth(thisMonth)[15];
+var d17 = fillMonth(thisMonth)[16];
+var d18 = fillMonth(thisMonth)[17];
+var d19 = fillMonth(thisMonth)[18];
+var d20 = fillMonth(thisMonth)[19];
+var d21 = fillMonth(thisMonth)[20];
+var d22 = fillMonth(thisMonth)[21];
+var d23 = fillMonth(thisMonth)[22];
+var d24 = fillMonth(thisMonth)[23];
+var d25 = fillMonth(thisMonth)[24];
+var d26 = fillMonth(thisMonth)[25];
+var d27 = fillMonth(thisMonth)[26];
+var d28 = fillMonth(thisMonth)[27];
+var d29 = fillMonth(thisMonth)[28];
+var d30 = fillMonth(thisMonth)[29];
+var d31 = fillMonth(thisMonth)[30];
+var d32 = fillMonth(thisMonth)[31];
+var d33 = fillMonth(thisMonth)[32];
+var d34 = fillMonth(thisMonth)[33];
+var d35 = fillMonth(thisMonth)[34];
+
+d1.st = 1;
+
+d31.st = 31;
+
+
+function fillMonth(x) {
+  if (x == 9) {
+    return oct19;
+  } else if (x == 10) {
+    return nov19;
   } else if (x == 11) {
-    return duDec19 + 1;
+    return dec19;
   }
 }
 
-// x = day box number;
-function firstDay(x) {
-  return cy19[firstBox(thisMonth) + x];
+function highLight(x, y) {
+  if (x == today) {
+    y.style.backgroundColor = "white";
+  } else {
+    y.style.backgroundColor = "blue";
+  }
+}
+
+function cancelOne(x, y, z) {
+  if ((x + y) > 50) {
+    z.style.backgroundColor = "black";
+  }
 }
