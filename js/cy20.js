5ddee294318887ae7c8c9f9d974b47202d1533f5
ekfitz <51545728+cultofsnake@users.noreply.github.com>
2019-10-30 11:59:48 -0700

    fully functioning durrent month with today highlight

--- js/cy20.js
+++ js/cy20.js
@@ -30,6 +30,8 @@ var dOy = [0,
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
 ];
 
+var wd = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
 var jan19 = cy19.slice(1, 34);
 var feb19 = cy19.slice(27, 62);
 var mar19 = cy19.slice(55, 90);
@@ -111,10 +113,79 @@ var d33 = fillMonth(thisMonth)[32];
 var d34 = fillMonth(thisMonth)[33];
 var d35 = fillMonth(thisMonth)[34];
 
-d1.st = 1;
+var m1 = findMonth(d1);
+var m2 = findMonth(d2);
+var m3 = findMonth(d3);
+var m4 = findMonth(d4);
+var m5 = findMonth(d5);
+var m6 = findMonth(d6);
+var m7 = findMonth(d7);
+var m8 = findMonth(d8);
+var m9 = findMonth(d9);
+var m10 = findMonth(d10);
+var m11 = findMonth(d11);
+var m12 = findMonth(d12);
+var m13 = findMonth(d13);
+var m14 = findMonth(d14);
+var m15 = findMonth(d15);
+var m16 = findMonth(d16);
+var m17 = findMonth(d17);
+var m18 = findMonth(d18);
+var m19 = findMonth(d19);
+var m20 = findMonth(d20);
+var m21 = findMonth(d21);
+var m22 = findMonth(d22);
+var m23 = findMonth(d23);
+var m24 = findMonth(d24);
+var m25 = findMonth(d25);
+var m26 = findMonth(d26);
+var m27 = findMonth(d27);
+var m28 = findMonth(d28);
+var m29 = findMonth(d29);
+var m30 = findMonth(d30);
+var m31 = findMonth(d31);
+var m32 = findMonth(d32);
+var m33 = findMonth(d33);
+var m34 = findMonth(d34);
+var m35 = findMonth(d35);
 
-d31.st = 31;
+dbox1.dow = wd[0];
+dbox2.dow = wd[1];
+dbox3.dow = wd[2];
+dbox4.dow = wd[3];
+dbox5.dow = wd[4];
+dbox6.dow = wd[5];
+dbox7.dow = wd[6];
+dbox8.dow = wd[0];
+dbox9.dow = wd[1];
+dbox10.dow = wd[2];
+dbox11.dow = wd[3];
+dbox12.dow = wd[4];
+dbox13.dow = wd[5];
+dbox14.dow = wd[6];
+dbox15.dow = wd[0];
+dbox16.dow = wd[1];
+dbox17.dow = wd[2];
+dbox18.dow = wd[3];
+dbox19.dow = wd[4];
+dbox20.dow = wd[5];
+dbox21.dow = wd[6];
+dbox22.dow = wd[0];
+dbox23.dow = wd[1];
+dbox24.dow = wd[2];
+dbox25.dow = wd[3];
+dbox26.dow = wd[4];
+dbox27.dow = wd[5];
+dbox28.dow = wd[6];
+dbox29.dow = wd[0];
+dbox30.dow = wd[1];
+dbox31.dow = wd[2];
+dbox32.dow = wd[3];
+dbox33.dow = wd[4];
+dbox34.dow = wd[5];
+dbox35.dow = wd[6];
 
+//var mc1 = monthCompare(thisMonth, m1, dbox1);
 
 function fillMonth(x) {
   if (x == 9) {
@@ -126,16 +197,41 @@ function fillMonth(x) {
   }
 }
 
-function highLight(x, y) {
-  if (x == today) {
-    y.style.backgroundColor = "white";
-  } else {
-    y.style.backgroundColor = "blue";
+function assBox(x) {
+  if (x.innerHTML == today) {
+    return "purple";
   }
 }
 
-function cancelOne(x, y, z) {
-  if ((x + y) > 50) {
-    z.style.backgroundColor = "black";
+function findMonth(x) {
+  if (x > 20) {
+    return thisMonth - 1;
+  } else {
+    return thisMonth;
+  }
+}
+
+//only applies to first two rows of month;
+// x = thisMonth, y = m1, z = dbox1, za = dtext1;
+function monthCompare(x, y, z, za) {
+  if (x !== y) {
+    z.style.backgroundColor = "transparent";
+  } else {
+    z.style.backgroundColor = assBox(za);
+  }
+}
+
+function setOPA(x) {
+  if (x.dow == wd[0] || x.dow == wd[6]) {
+    x.style.opacity = "0.6";
+  }
+}
+
+// x = dbox1, y = dtext1;
+function largeFont(x, y) {
+  if (x.style.backgroundColor == "purple") {
+    y.style.fontSize = "2em";
+  } else {
+    y.style.fontSize = "1em";
   }
 }
